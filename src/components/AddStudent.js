12b10f849ca4e10f483b7990f9d8aa04738d0f42
Kur0a5h <kuroash.esmaili@gmail.com>
2019-03-28 23:22:35 -0700

    created file structure, installed mobx and react-mobx, set up addstudent form component, updated app.js

---
+++ src/components/AddStudent.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import {
+  Form, Button, Header, Message,
+} from 'semantic-ui-react';
+import { observable, action, computed} from 'mobx';
+import { observer, inject } from 'mobx-react';
+
+@inject('Firebase')
+class AddStudent extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  @observable
+  inputFields = {
+    name: '',
+    course: '',
+    grade: ''
+  }
+
+  @observable
+  nameError = {
+    error: false,
+    msg: null
+  }
+
+  @observable
+  courseError = {
+    error: false,
+    msg: null
+  }
+
+  @observable
+  gradeError = {
+    error: false,
+    msg: null
+  }
+
+  @action
+  clearBtnHandler = () => {
+    this.clearInputFields();
+  }
+
+  @action
+  submitBtnHandler = (e) => {
+    e.preventDefault();
+
+    if (this.checkInput()){
+      this.props.Firebase.addStudentToServer(
+        this.inputFields.name.trim(), 
+        this.inputFields.course.trim(), 
+        Number(this.inputFields.grade)
+      );
+  
+      this.clearInputFields();
+      if (this.props.closeModal) this.props.closeModal();
+    }
+  }
+
+  @action
+  checkInput = () => {
+    if (this.inputFields.name.trim() === ''){
+      this.nameError.error = true;
+      this.nameError.msg = "Name cannot be empty";
+    } else {
+      this.nameError.error = false;
+      this.nameError.msg = null;
+    }
+    
+    if (this.inputFields.course.trim() === ''){
+      this.courseError.error = true;
+      this.courseError.msg = "Course cannot be empty";
+    } else {
+      this.courseError.error = false;
+      this.courseError.msg = null;
+    }
+    
+    if (this.inputFields.grade.trim() === ''){
+      this.gradeError.error = true;
+      this.gradeError.msg = "Grade cannot be empty";
+    } else if (isNaN(Number(this.inputFields.grade))){
+      this.gradeError.error = true;
+      this.gradeError.msg = "Grade has to be a number";
+    } else if (Number(this.inputFields.grade > 100)){
+      this.gradeError.error = true;
+      this.gradeError.msg = "Grade cannot be over 100";
+    } else if (Number(this.inputFields.grade < 0)){
+      this.gradeError.error = true;
+      this.gradeError.msg = "Grade cannot be below 0";
+    } else {
+      this.gradeError.error = false;
+      this.gradeError.msg = null;
+    }
+
+    if (!this.gradeError.error && !this.nameError.error && !this.courseError.error){
+      return true; // passes the input check
+    } else {
+      return false; //fails the input check
+    }
+  }
+
+  @action
+  clearInputFields() {
+    this.inputFields = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+
+    this.clearErrorFlags();
+  }
+
+  @action
+  clearErrorFlags() {
+    this.nameError.error = false;
+    this.nameError.msg = null;
+    this.gradeError.error = false;
+    this.gradeError.msg = null;
+    this.courseError.error = false;
+    this.courseError.msg = null;
+  }
+
+  @action
+  handleChange = event => {
+    this.inputFields[event.target.name] = event.target.value;
+  }
+
+  @observer
+  render() {
+    return (
+        <Form error>
+          <Header>Add Student</Header>
+          <Form.Field required>
+            <label>Name</label>
+            <input placeholder='Student Name' name='name' value={this.inputFields.name} onChange={this.handleChange}/>
+            <Message error content={this.nameError.msg}/>
+          </Form.Field>
+          <Form.Field required>
+            <label>Course</label>
+            <input placeholder='Course' name='course' value={this.inputFields.course} onChange={this.handleChange}/>
+            <Message error content={this.courseError.msg}/>
+          </Form.Field>
+          <Form.Field required>
+            <label>Grade</label>
+            <input placeholder='Grade' name='grade' value={this.inputFields.grade} onChange={this.handleChange}/>
+            <Message error content={this.gradeError.msg}/>
+          </Form.Field>
+          <Button primary onClick={this.submitBtnHandler}>Submit</Button>
+          <Button secondary onClick={this.clearBtnHandler}>Clear</Button>
+        </Form>
+    );
+  }
+}
+
+export default AddStudent;
---
+++ src/components/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import {
+  Container, Grid, Responsive, Button, Modal
+} from 'semantic-ui-react';
+import { observable, action, computed } from 'mobx';
+import { observer, inject } from 'mobx-react';
+import Table from './Table';
+import AddStudent from './AddStudent';
+import Registration from './Registration';
+import Header from './Header.js';
+import '../assets/css/App.css';
+
+@inject ('Firebase')
+@observer
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  @computed
+  get loginState() {
+    return this.props.Firebase.user;
+  }
+
+  @observable
+  modalOpen = false;
+
+  @action
+  closeModal = () => {
+    this.modalOpen = false;
+  }
+
+  @action
+  openModal = () => {
+    this.modalOpen = true;
+  }
+
+  @computed
+  get loggedin() {
+    return (
+      <Container>
+        <Header />
+        <Grid>
+          <Grid.Row columns={2}>
+            <Grid.Column computer={12} mobile={16} tablet={16}>
+              <Table />
+            </Grid.Column>
+            <Responsive as={Grid.Column} width={4} minWidth={992}>
+              <AddStudent />
+            </Responsive>
+          </Grid.Row>
+        </Grid>
+        <Responsive 
+          as={Modal}
+          closeIcon
+          trigger={<Button primary open={this.modalOpen} onClick={this.openModal} className='rAddStudentButton'>Add Student</Button>}
+          maxWidth={991}
+          open={this.modalOpen}
+          onClose={this.closeModal}
+        >
+          <Modal.Content>
+            <AddStudent closeModal={this.closeModal}/>
+          </Modal.Content>
+        </Responsive>
+      </Container>
+  )};
+
+  render() {
+    return this.loginState ? this.loggedin : <Registration />;
+  }
+}
+
+export default App;
